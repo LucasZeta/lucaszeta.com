b2462fb3b5d1f4649f1aa644aba21e569c92ce3f
Lucas Souza <lucasdsdc@gmail.com>
2016-01-01 22:28:51 -0200

    Added filtering

--- taco/app.js
+++ taco/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('taco', []);
 
 app.controller('AlimentosCtrl', function($scope, $http) {
+    $scope.searchFilter = '';
+
     $http.get('dados/conteudo.json')
         .success(function(data, status, headers, config) {
             $scope.alimentos = data;
